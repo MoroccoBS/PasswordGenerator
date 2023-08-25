6a7f60af635f6c535062c9fbbc259b3983005426
MrBs <Chouaibwidadipur@gmail.com>
2023-08-25 15:16:44 +0100

    Fixed Images not Loading

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import Auth from "../Auth";
+import LoadingImage from "../assets/images/Loading.png";
 
 function SignIn() {
   return (
@@ -7,7 +8,7 @@ function SignIn() {
         <Auth />
         <div className="w-full h-full absolute bg-black/20 z-10"></div>
         <img
-          src="src/assets/images/Loading.png"
+          src={LoadingImage}
           className="object-contain w-1/2 h-full lg:relative absolute"
           alt=""
         />
